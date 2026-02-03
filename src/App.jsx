f7a8e71ab247ecf7ac2685aaf40c30439b4eb318
Ernesto <ernesto.scorzelli77@gmail.com>
2026-02-03 10:14:55 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Button from "./components/ButtonComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [count, setCount] = useState(0);
